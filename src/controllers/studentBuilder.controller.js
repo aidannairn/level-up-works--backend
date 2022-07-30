0b8eb521c0dc423752066ee73ec3530527f69d11
aidannairn <aidan.nairn95@gmail.com>
2022-07-31 07:54:28 +1200

    feat: Use getProjectItems to query database to send current project columns and join instructions.

--- src/controllers/studentBuilder.controller.js
+++ src/controllers/studentBuilder.controller.js
@@ -1,5 +1,42 @@
-const getProjectItems = (req, res) => {
-  console.log('Student Builder Controller')
+const connection = require('../config/database')
+const env = require('../config/dotenv')
+
+const getProjectItems = async (req, res) => {
+  const projectID = req.params.projectID
+
+  const db = env('db')
+
+  try {
+    const [projects] = await connection.query(`
+    SELECT
+      Projects.id AS projectID,
+      Projects.project_name AS projectName,
+      Projects.video_link AS videoLink,
+      Projects.activity_type AS activityType,
+      Projects.year_level AS yearLevel,
+      Projects.difficulty,
+      Projects.subscription,
+      Projects.subject_matter AS subjectMatter,
+
+      JSON_ARRAYAGG(
+        JSON_OBJECT(
+          'pageID', Instruction_Elements.page_id,
+          'elementType', Instruction_Elements.element_type,
+          'elementContent', Instruction_Elements.element_content
+        )
+      ) AS instructions
+    
+    FROM ${db}.Projects
+
+    JOIN ${db}.Instruction_Elements ON Instruction_Elements.project_id = ${projectID}
+
+    WHERE ${db}.Projects.id = ?`, [projectID])
+
+    res.status(200).json(projects[0])
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = { getProjectItems }
